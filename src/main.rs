bdfcdd2c3c7e7581681031374da8d4e5ab96c1c9
Fahim (MacBook) <psymh15@nottingham.ac.uk>
2025-01-01 20:58:23 +0000

    Flesh out what a function would be

--- src/main.rs
+++ src/main.rs
@@ -10,13 +10,30 @@ pub struct BasicBlock {
 
 #[derive(Debug)]
 pub enum Instruction {
-    Return { val: u64 },
+    Constant { val: u64 },
+    Binary { op: BinOp, lhs: usize, rhs: usize },
+    Return { val: usize },
+}
+
+#[derive(Debug)]
+pub enum BinOp {
+    Add,
 }
 
 fn main() {
+    // In code, this would be ```fn calculate() { return 9 + 10; }````
     let func = Function {
         blocks: vec![BasicBlock {
-            instructions: vec![Instruction::Return { val: 0 }],
+            instructions: vec![
+                Instruction::Constant { val: 9 },
+                Instruction::Constant { val: 10 },
+                Instruction::Binary {
+                    op: BinOp::Add,
+                    lhs: 0,
+                    rhs: 1,
+                },
+                Instruction::Return { val: 2 },
+            ],
         }],
     };
 
